80b0077206e88e77bf32b460e60e5d1507e8052d
alexwebgr <websurfer992@gmail.com>
2016-08-25 12:26:25 +0100

    initial structure

---
+++ grunt/wiredep.js
@@ -0,0 +1,16 @@
+/**
+ * @packageName grunt-wiredep
+ * @description Grunt Task for Injecting the Bower Dependencies in the .html file
+ * @url https://www.npmjs.com/package/grunt-wiredep
+ */
+
+module.exports = {
+    main: {
+        src: [
+            "public/index.html"
+        ],
+
+        options: {
+        }
+    }
+};
